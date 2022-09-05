7229c5b4319dd6eecf7e74bc36166ab3007ae0a1
Isabela Stein <112905211+Isabelastein@users.noreply.github.com>
2022-09-05 20:50:26 -0300

    Update mural novo.py

--- mural novo.py
+++ mural novo.py
@@ -2,6 +2,5 @@ from time import sleep
 
 print("Felipe Paixão")
 print("Espero que eu já esteja para virar um cientista de dados!")
-print("Júlio César")
-print("Esperto que eu esteja empregado em uma empresa de TI até o final do curso!")
+print("Que tenhamos todos uma ótima cerreira e muito sucesso -Isabela Stein!")
 sleep(1)
